2ab0fc853968efaae9909924149884addb2c2ec8
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-04-04 23:53:00 +0300

    add new features

--- projects-udemy/Bankist/script.js
+++ projects-udemy/Bankist/script.js
@@ -68,12 +68,12 @@ const displayMovements = function (movements) {
 
   movements.forEach((mov, i) => {
 
-    const type = mov > 0 ? 'deposit' : 'withdrawl';
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = ` 
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
   </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -82,6 +82,52 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const calcDisplayBalance = (movements) => {
+  const balance = movements.reduce((acc, cur) => acc + cur, 0);
+
+  labelBalance.textContent = `${balance}€`;
+};
+
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = (movements) => {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + cur, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const output = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + cur, 0);
+  labelSumOut.textContent = `${Math.abs(output)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => deposit * 1.2 / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, int) => acc + cur, 0);
+
+  labelSumInterest.textContent = `${interest}€`;
+
+};
+calcDisplaySummary(account1.movements);
+
+
+const createUsername = (accs) => {
+  accs.forEach(acc => acc.username = acc.owner
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word[0])
+    .join(''));
+};
+
+
+createUsername(accounts);
+
+
+
+
 
 
 /////////////////////////////////////////////////
@@ -96,4 +142,10 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const deposits = movements.filter(mov => mov > 0);
+
+const withdrawals = movements.filter(mov => mov < 0);
+
 /////////////////////////////////////////////////
+
+;;
