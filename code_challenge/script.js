ffea65fe777014854ffb37b9d5b8531b09a587b1
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-04-04 23:52:45 +0300

    add Coding Challenge #3

--- code_challenge/script.js
+++ code_challenge/script.js
@@ -395,7 +395,7 @@ function checkDogs(dogsJulia, dogsKate) {
 
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
-//Coding Challenge #2
+//Coding Challenge #2  Coding Challenge #3
 
 function calcAverageHumanAge(ages) {
     // const dogsAge = ages.map(age => age <= 2 ? 2 * age : 16 + age * 4).filter(age => age >= 18);
@@ -409,3 +409,6 @@ function calcAverageHumanAge(ages) {
 }
 
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+
+
