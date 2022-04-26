2cd1ae371f749484fe45c701df488f5e49c523e8
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-04-26 12:49:59 +0300

    add last oop challenge

--- code_challenge/script.js
+++ code_challenge/script.js
@@ -516,6 +516,7 @@ class CarCl {
     brake() {
         this.speed -= 5;
         console.log(`${this.make} is going at ${this.speed} km/h`);
+        return this;
     }
 
     get speedUS() {
@@ -532,3 +533,60 @@ const ford = new CarCl('Ford', 120);
 console.log(ford.speedUS);
 ford.speedUS = 50;
 ford.accelerate();
+
+// Coding Challenge #3
+
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+    this.speed += 20;
+    this.charge--;
+    console.log(`Tesla going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+};
+
+
+
+const electrocar = new EV('Tesla', 120, 23);
+console.log(electrocar);
+
+electrocar.brake();
+electrocar.accelerate();
+electrocar.chargeBattery(90);
+electrocar.accelerate();
+console.log(electrocar);
+
+// Coding Challenge #4
+
+class EVCl extends CarCl {
+
+    #charge;
+
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.#charge = charge;
+    }
+
+    chargeBattery(chargeTo) {
+        this.#charge = chargeTo;
+        return this;
+    }
+    accelerate() {
+        this.speed += 20;
+        this.#charge--;
+        console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.#charge}%`);
+        return this;
+    };
+}
+console.log('================================');
+const rivian = new EVCl('Rivian', 120, 23);
+
+rivian.accelerate().accelerate().brake().chargeBattery(90).accelerate();
